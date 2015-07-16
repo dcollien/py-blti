a27907c4d2a221982f9c78d40c95d694500cc6dc
David Collien <david@openlearning.com>
2015-07-16 18:32:29 +1000

    update requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='blti',
-   version='0.1.4',
+   version='0.1.5',
    py_modules=['blti'],
    author='David Collien',
    author_email='me@dcollien.com',
@@ -11,4 +11,5 @@ setup(
    description='py-blti: basic LTI authentication for python/django',
    long_description='py-blti: A basic LTI Provider authentication decorator for django, and basic LTI Consumer signing function.',
    platforms=['any'],
+   install_requires=['oauth2']
 )
